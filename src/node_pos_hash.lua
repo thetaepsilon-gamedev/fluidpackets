26afbd1798acd5f6315462c8f223fb76b87d98db
thetaepsilon <thetaepsilon@noreply.users.github.com>
2018-10-09 16:10:45 +0100

    src: node_pos_hash.lua: [=] add rational and usage comments

--- src/node_pos_hash.lua
+++ src/node_pos_hash.lua
@@ -1,3 +1,28 @@
+--[[
+A convention used in this mod, similar to that used in libmt_node_network,
+is to represent sets of packets as a table mapping strings to packet structs:
+{
+	["(0,8,2)"] = {
+		x = 0,
+		y = 8,
+		z = 2,
+		-- additional keys for packet here, such as volume
+	},
+	...
+}
+The internal position of a packet is turned into a well-known string when inserted.
+This enables fast checking for an existing packet at a given position,
+important when carrying out packet merges;
+an existing packet will get added to, otherwise a packet is moved or created.
+
+The hash function defined here is the common definition of the string form.
+To create a packet set, take a table and some packets to insert,
+then for each packet (which are assumed to be at unique positions)
+let k = hash(packet), then insert the packet as table[k] = packet.
+]]
+
+
+
 local isint = _mod.util.math.isint
 local hash = function(pos)
 	assert(isint(pos.x))
