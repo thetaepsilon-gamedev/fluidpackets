933c17ef7a1460cdcd43da881debd4bb2203eead
thetaepsilon <thetaepsilon@noreply.users.github.com>
2019-02-17 19:25:17 +0000

    src: fluid_packet_batch.lua: [~] remove currently unreferenced default callbacks (to be re-added elsewhere)

--- src/fluid_packet_batch.lua
+++ src/fluid_packet_batch.lua
@@ -88,54 +88,6 @@ local i = {}
 
 
 
--- these are the default callbacks used for various operations below.
-local null = _mod.util.callbacks.dummies.null
-local const = _mod.util.callbacks.dummies.const_
-local defcallbacks = {
-	-- invoked when a packet ends up inside an inappropriate block.
-	-- packet will be destroyed upon return.
-	-- default: do nothing extra
-	on_packet_destroyed = null,
-	-- invoked to handle condition of attempting to insert volume
-	-- into a node that isn't a bearer.
-	-- passed target position, node, and would-be inserted volume;
-	-- returns either a remainder of the consumed volume,
-	-- or nil to indicate it couldn't be handled.
-	on_escape = const(nil),
-
-	-- called to retrieve the pipe definition of a given node, if any.
-	-- this is used to allow change in definition format,
-	-- including possible support for multiple liquid types in future.
-	lookup_definition = nil,
-
-	-- called when a packet is found in an unloaded area:
-	-- provided packet and hash.
-	-- the callback returns a truth value indicating whether it handled this;
-	-- if and only if this is true, the callback may "consume" the packet,
-	-- as it will be detached from the packet map.
-	-- a false value will leave the packet where it is, unable to move.
-	-- the default is to do nothing and leave the packet alone.
-	on_packet_unloaded = const(false),
-
-	-- called when a live packet doesn't exist yet at a target position.
-	-- this is usually when some volume wants to be inserted there.
-	-- if a node is loaded but no packet exists yet there,
-	-- this callback is given the opportunity to return a packet set,
-	-- which will be merged into the packet map.
-	-- this can be used to ensure packets are re-loaded before merges,
-	-- however some other mechanism for loading (like LBMs) is advised;
-	-- this is simply to work around a weak ordering issue with LBMs.
-	-- this function is passed position and hash,
-	-- and may either return nil or a packet set.
-	-- note that this packet set is not required to include the target pos.
-	-- the default is to do nothing here.
-	on_packet_load_hint = const(nil),
-}
-
-
-
-
-
 -- load node at position and fetch it's bearer definition, if any
 local get_node_and_def = _mod.m.bearer_def.get_node_and_def
 
