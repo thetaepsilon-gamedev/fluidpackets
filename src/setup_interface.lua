f360eebf7ebceab2cdc5772ea1ba967ff3ae537c
thetaepsilon <thetaepsilon@noreply.users.github.com>
2019-02-24 17:55:18 +0000

    src: [*] always export entire types table

--- src/setup_interface.lua
+++ src/setup_interface.lua
@@ -8,8 +8,7 @@ i.util.bearer_def = _mod.util.bearer_def
 i.util.bearer_helpers = _mod.m.bearer_helpers
 i.callbacks = {}
 i.callbacks.suspend = _mod.m.suspend
-i.types = {}
-i.types.IBatchRunnerCallbacks = _mod.types.IBatchRunnerCallbacks
+i.types = _mod.types
 
 
 
